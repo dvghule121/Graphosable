51ae0a184fb576e4339a62ff9e988547ce86ff36
Dyno <dvghule121@gmail.com>
2023-09-20 22:59:45 +0530

    version jdk 17 bbuild it successfully

--- app/src/main/java/com/dynocodes/graphosable/MainActivity.kt
+++ app/src/main/java/com/dynocodes/graphosable/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.dynocodes.graphosable.ui.theme.GraphosableTheme
 
 
@@ -22,19 +25,19 @@ class MainActivity : ComponentActivity() {
 
 
                     ) {
-//                    val list = ArrayList<BarData>()
-//                    for (i in 1..7) {
-//                        list.add(BarData(i.toFloat(), "$i"))
-//                    }
-//                    Graphs().PieChartWithLabels(
-//                        modifier = Modifier.fillMaxWidth()
-//                            .height(350.dp),
-//                        data = listOf(
-//                            Slice(120, "Steel"),
-//                            Slice(90, "Steel"),
-//                            Slice(40, "Steel")
-//                        ), context = this, ringSize = 50f
-//                    )
+                    val list = ArrayList<BarData>()
+                    for (i in 1..7) {
+                        list.add(BarData(i.toFloat(), "$i"))
+                    }
+                    Graphs().PieChartWithLabels(
+                        modifier = Modifier.fillMaxWidth()
+                            .height(350.dp),
+                        data = listOf(
+                            Slice(120, "Steel"),
+                            Slice(90, "Steel"),
+                            Slice(40, "Steel")
+                        ), context = this, ringSize = 50f
+                    )
 
                 }
             }
