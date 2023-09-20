09d5aeb8564acfb50cfd9646361a6598544d8c60
Dyno <dvghule121@gmail.com>
2023-09-20 22:27:37 +0530

    version jdk 17 bbuild it successfully

--- graphosable_lib/build.gradle.kts
+++ graphosable_lib/build.gradle.kts
@@ -26,6 +26,8 @@ android {
         }
     }
 
+
+
     buildFeatures {
         compose = true
     }
@@ -43,10 +45,13 @@ android {
 
 
 
+
+
 }
 
 dependencies {
 
+    implementation(kotlin("stdlib"))
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
@@ -56,6 +61,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.android.support:support-annotations:28.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
